3a5a1b32fe806abd2c648944448fa9173331af89
Romero Galiza Jr <rgaliza@schubergphilis.com>
2018-05-10 19:09:41 +0200

    add suport to multiple versioning schemas

--- README.md
+++ README.md
@@ -17,6 +17,7 @@ resources:
   source:
     endpoint: http://ARTIFACTORY-HOST-NAME-GOES-HERE:8081/artifactory
     repository: "/repository-name/sub-folder"
+    version_schema: semver
     regex: "myapp-(?<version>.*).txt"
     username: YOUR-ARTIFACTORY-USERNAME
     password: YOUR-ARTIFACTORY-PASSWORD
@@ -25,20 +26,21 @@ resources:
 
 ## Source Configuration
 
-* `endpoint`: *Required.* The Artifactory REST API endpoint. eg. http://YOUR-HOST_NAME:8081/artifactory.  
-* `repository`: *Required.* The Artifactory repository which includes any folder path, must contain a leading '/'. ```eg. /generic/product/pcf```  
-* `regex`: *Required.* Regular expression used to extract artifact version, must contain 'version' group. ```E.g. myapp-(?<version>.*).tar.gz```  
-* `username`: *Optional.* Username for HTTP(S) auth when accessing an authenticated repository  
-* `password`: *Optional.* Password for HTTP(S) auth when accessing an authenticated repository  
-* `skip_ssl_verification`: *Optional.* Skip ssl verification when connecting to Artifactory's APIs. Values: ```true``` or ```false```(default).  
+* `endpoint`: *Required.* The Artifactory REST API endpoint. eg. http://YOUR-HOST_NAME:8081/artifactory.
+* `repository`: *Required.* The Artifactory repository which includes any folder path, must contain a leading '/'. ```eg. /generic/product/pcf```
+* `regex`: *Required.* Regular expression used to extract artifact version, must contain 'version' group. ```E.g. myapp-(?<version>.*).tar.gz```
+* `username`: *Optional.* Username for HTTP(S) auth when accessing an authenticated repository
+* `password`: *Optional.* Password for HTTP(S) auth when accessing an authenticated repository
+* `skip_ssl_verification`: *Optional.* Skip ssl verification when connecting to Artifactory's APIs. Values: ```true``` or ```false```(default).
+* `version_schema`: *Optional.* Specify the versioning schema. Values: ```erv``` or ```semver``` (default).
 
 ## Parameter Configuration
 
-* `file`: *Required for put* The file to upload to Artifactory  
-* `regex`: *Optional* overrides the source regex  
-* `folder`: *Optional.* appended to the repository in source - must start with forward slash /  
+* `file`: *Required for put* The file to upload to Artifactory
+* `regex`: *Optional* overrides the source regex
+* `folder`: *Optional.* appended to the repository in source - must start with forward slash /
 
-Saving/deploying an artifact to Artifactory in a pipeline job:  
+Saving/deploying an artifact to Artifactory in a pipeline job:
 
 ``` yaml
   jobs:
@@ -65,7 +67,7 @@ Saving/deploying an artifact to Artifactory in a pipeline job:
       params: { file: ./build/myapp-*.txt }
 ```
 
-Retrieving an artifact from Artifactory in a pipeline job:  
+Retrieving an artifact from Artifactory in a pipeline job:
 
 ``` yaml
 jobs:
@@ -88,7 +90,7 @@ jobs:
         - "Use file(s) from ./file-repository here..."
 ```
 
-See [pipeline.yml](https://github.com/pivotalservices/artifactory-resource/blob/master/pipeline.yml) for an example of a full pipeline definition file.  
+See [pipeline.yml](https://github.com/pivotalservices/artifactory-resource/blob/master/pipeline.yml) for an example of a full pipeline definition file.
 
 ## Resource behavior
 
--- assets/common.sh
+++ assets/common.sh
@@ -10,25 +10,62 @@ applyRegex_version() {
 }
 
 get_current_version() {
-  local regex=$1
-  local folder_contents=$2
+  local version_schema=$1
+  local regex=$2
+  # regex: myapp-(?<version>.*).txt
+  local folder_contents=$3
 
-  echo "$folder_contents" | jq --arg v "$regex" '[.children[].uri | capture($v)]' | jq 'sort_by(.version | split(".") | map(tonumber))' | jq '[.[length-1] | {version: .version}]'
+  case $version_schema in
+    semver)
+        echo "$folder_contents" | jq --arg v "$regex" '[.children[].uri | capture($v)]' | jq 'sort_by(.version | split(".") | map(tonumber))' | jq '[.[length-1] | {version: .version}]'
+        ;;
+    erv)
+        ## do something here
+        echo "$folder_contents" | jq --arg v "$regex" '[.children[].uri | capture($v)]' | jq 'sort_by(.version | split(":") | join("") | split("-") | map(tonumber))' | jq '[.[length-1] | {version: .version}]'
+        ;;
+    *)
+        echo "Invalid option"
+    ;;
+  esac
 }
 
 # Return all versions
 get_all_versions() {
-  local regex=$1
-  local folder_contents=$2
+  local version_schema=$1
+  local regex=$2
+  local folder_contents=$3
 
-  echo "$folder_contents" | jq --arg v "$regex" '[.children[].uri | capture($v)]' | jq 'sort_by(.version | split(".") | map(tonumber))' | jq '[.[] | {version: .version}]'
+  case $version_schema in
+    semver)
+        echo "$folder_contents" | jq --arg v "$regex" '[.children[].uri | capture($v)]' | jq 'sort_by(.version | split(".") | map(tonumber))' | jq '[.[] | {version: .version}]'
+        ;;
+    erv)
+        echo "$folder_contents" | jq --arg v "$regex" '[.children[].uri | capture($v)]' | jq 'sort_by(.version | split(":") | join("") | split("-") | map(tonumber))' | jq '[.[] | {version: .version}]'
+        ;;
+    *)
+        echo "Invalid option"
+    ;;
+  esac
 }
 
 get_files() {
-  local regex="(?<uri>$1)"
-  local folder_contents=$2
+  local version_schema=$1
+  local regex="(?<uri>$2)"
+  local folder_contents=$3
 
-  echo "$folder_contents" | jq --arg v "$regex" '[.children[].uri | capture($v)]' | jq 'sort_by(.version  | split(".") | map(tonumber))' | jq '[.[] | {uri: .uri, version: .version}]'
+
+  case $version_schema in
+    semver)
+        echo "$folder_contents" | jq --arg v "$regex" '[.children[].uri | capture($v)]' | jq 'sort_by(.version  | split(".") | map(tonumber))' | jq '[.[] | {uri: .uri, version: .version}]'
+        ;;
+    erv)
+        echo "$folder_contents" | jq --arg v "$regex" '[.children[].uri | capture($v)]' | jq 'sort_by(.version  | split(":") | join("") | split("-") | map(tonumber))' | jq '[.[] | {uri: .uri, version: .version}]'
+        ;;
+    *)
+        echo "Invalid option"
+    ;;
+  esac
+
 }
 
 artifactory_get_folder_contents() {
@@ -41,11 +78,12 @@ artifactory_get_folder_contents() {
 #     regex=ecd-front-(?<version>.*).tar.gz
 artifactory_current_version() {
   local artifacts_url=$1
-  local regex=$2
+  local version_schema=$2
+  local regex=$3
 
   local folder_contents=$(artifactory_get_folder_contents "$1")
 
-  get_current_version "$regex" "$folder_contents"
+  get_current_version "$version_schema" "$regex" "$folder_contents"
 
 }
 
@@ -53,31 +91,34 @@ artifactory_current_version() {
 # Return all versions
 artifactory_versions() {
   local artifacts_url=$1
-  local regex=$2
+  local version_schema=$2
+  local regex=$3
 
   local folder_contents=$(artifactory_get_folder_contents "$1")
 
-  get_all_versions "$regex" "$folder_contents"
+  get_all_versions "$version_schema" "$regex" "$folder_contents"
 
 }
 
 # return uri and version of all files
 artifactory_files() {
   local artifacts_url=$1
-  local regex=$2
+  local version_schema=$2
+  local regex=$3
 
   local folder_contents=$(artifactory_get_folder_contents "$1")
 
-  get_files "$regex" "$folder_contents"
+  get_files "$version_schema" "$regex" "$folder_contents"
 
 }
 
 in_file_with_version() {
   local artifacts_url=$1
-  local regex="(?<uri>$2)"
-  local version=$3
+  local version_schema=$2
+  local regex="(?<uri>$3)"
+  local version=$4
 
-  result=$(artifactory_files "$artifacts_url" "$regex")
+  result=$(artifactory_files "$artifacts_url" "$version_schema" "$regex")
   echo $result | jq --arg v "$version" '[foreach .[] as $item ([]; $item ; if $item.version == $v then $item else empty end)]'
 
 }
@@ -86,10 +127,11 @@ in_file_with_version() {
 # return the list of versions from provided version
 check_version() {
   local artifacts_url=$1
-  local regex=$2
+  local version_schema=$2
+  local regex=$3
   local version=$3
 
-  result=$(artifactory_versions "$artifacts_url" "$regex")
+  result=$(artifactory_versions "$artifacts_url" "$version_schema" "$regex")
   echo $result | jq --arg v "$version" '[foreach .[] as $item ([]; $item ; if $item.version >= $v then $item else empty end)]'
 
 }
